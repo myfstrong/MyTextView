74c7b37d442f71c8d9e1311b5453d429fc91860e
myfstrong <4821825@qq.com>
2016-02-13 15:03:09 +0800

    更新：实例3

--- app/src/main/java/com/example/myfstrong/mytextview/MainActivity.java
+++ app/src/main/java/com/example/myfstrong/mytextview/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myfstrong.mytextview;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     String s1;
     String s2;
+    String s3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         tv1.setText(s1);
 
-        //实例2：利用HTML实现超链接
+        //实例2：利用HTML标签实现超链接
         s2 = "<font color='red'>我的主页是：</font>";
         s2 += "<a href='http://www.baidu.com'>百度</a>";
 
@@ -44,5 +48,35 @@ public class MainActivity extends AppCompatActivity {
 
         tv2.setText(spanned);
         tv2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        //实例3：利用HTML标签插入图片
+        s3 = "图标：<img src='ic_launcher'/>";
+
+        //步骤1：实现字符串s3的HTML标签化，并赋值给spanned1（跨分区卷）
+        Spanned spanned1 = Html.fromHtml(s3, new Html.ImageGetter() {
+            @Override
+            public Drawable getDrawable(String src) {
+
+                Drawable drawable = null;
+                try {
+                    Field field = R.drawable.class.getField(src);
+                    int resourceId = Integer.parseInt(field.get(null).toString());
+                    drawable = getResources().getDrawable(resourceId);
+                    drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+                return drawable;
+            }
+        }, null);
+
+        //步骤2：设置TextView的内容
+        tv3.setText(spanned1);
     }
 }
